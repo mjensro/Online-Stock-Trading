71d3e37fc688ab44683c51fe4e55e75314229f39
taywil612 <114127446+taywil612@users.noreply.github.com>
2023-02-03 20:03:08 -0500

    started server

---
+++ Server.py
@@ -0,0 +1,8 @@
+#Author/Collaborators: Taylor Williams, Michelle Sroka, Alexis Whisnant
+#Creation Date: 02/03/2023
+#Last Modification Date: 02/03/2023
+#Purpose: This is the Server program for a Online Stock Trading System. This Server program is
+#expected to communicate with an aligining Client program using TCP sockets. One active
+#client should be allowed to connect to the server.
+
+import socket
