0a4ed0660183dc12e2f446dd9dbff8ff1639c1d2
Michelle <michellesroka0@gmail.com>
2023-02-08 19:31:14 -0500

    Server code draft

--- Client.py
+++ Client.py
@@ -5,4 +5,14 @@
 #expected to communicate with an aligining Client program using TCP sockets. One active
 #client should be allowed to connect to the server.
 
+#Following this tutorial : https://www.youtube.com/watch?v=6sHGBXwkFQU
 import socket
+import sys
+
+ip = ''
+port = 7399 #last 4 digits of id for unique port
+server = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+server.connect((ip,port))
+
+
+#if __name__ == "__main__":
--- Server.py
+++ Server.py
@@ -6,3 +6,40 @@
 #client should be allowed to connect to the server.
 
 import socket
+import sys
+from _thread import *
+
+#if __name__ == "__main__":
+
+ip = "127.0.0.1"
+port = 7399
+server = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    server.bind((ip,port)) 
+except socket.error as e:
+    print(str(e))
+
+server.listen(5)
+print('Waiting for connection...')
+
+def threaded_client(conn):
+    conn.send(str.encode('Welcome, type your info\n'))
+
+    while True: #keeps thread active as long as the user doesn't exit
+        data = conn.recv(2048) #buffer rate
+        reply = 'Server output: '+ data.decode('utf-8')
+        if not data:
+            break
+        conn.sendall(str.encode(reply))
+    conn.close()
+
+while True: #starting new thread
+    conn, addr = server.accept()
+    print('connected to: '+addr[0]+':'+str(addr[1]))
+    
+    start_new_thread(threaded_client,(conn,))
+#client, address = server.accept()
+#print(f"Connection Established -  {address[0]}:{address[1]}")
+
+#print("Client Disconnected")
